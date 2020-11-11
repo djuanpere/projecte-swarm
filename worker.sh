00cbc0d0685ae8e78112711e25b76908db1c874e
root <root@maquina-4.europe-west1-b.c.cosmic-palace-293407.internal>
2020-11-11 21:17:28 +0000

    worker ok

--- worker.sh
+++ worker.sh
@@ -1,12 +1,13 @@
-apt install -y git
-curl https://get.docker.com | sudo bash
+#run by hand next two lines
+#apt install -y git
+#curl https://get.docker.com | sudo bash
 
 mkdir -p /srv/docker
-#	Configurar NFS
-echo 'node-1:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
+#Configurar NFS
+echo 'maquina-4:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
 apt install -y nfs-common
 echo '/srv/nfs 10.132.0.0/24(rw,no_root_squash,no_subtree_check)' >> /etc/exports
 mount -a
-
+chmod 775 /srv/docker/join.sh
 /srv/docker/join.sh
 
