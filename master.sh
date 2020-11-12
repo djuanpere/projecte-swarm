87a9df77e2679a79ba1c307b61b5ce7d0e46a49a
root <root@maquina-4.europe-west1-b.c.cosmic-palace-293407.internal>
2020-11-12 17:23:09 +0000

    master OK

--- master.sh
+++ master.sh
@@ -7,7 +7,7 @@ mkdir -p /srv/nfs
 #	Configurar NFS
 echo 'maquina-4:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
 apt install -y nfs-kernel-server
-echo '/srv/nfs 10.132.0.5/32(rw,no_root_squash,no_subtree_check)' >> /etc/exports
+echo '/srv/nfs 10.132.0.0/24(rw,no_root_squash,no_subtree_check)' >> /etc/exports
 systemctl start nfs-kernel-server
 exportfs -r
 mount -a
