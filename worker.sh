a083f344c2566955d8c38e13d80fd27182a2cfd4
root <root@maquina-4.europe-west1-b.c.cosmic-palace-293407.internal>
2020-11-12 16:59:16 +0000

    worker ok

--- worker.sh
+++ worker.sh
@@ -6,7 +6,7 @@ mkdir -p /srv/docker
 #Configurar NFS
 echo 'maquina-4:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
 apt install -y nfs-common
-echo '/srv/nfs 10.132.0.0/24(rw,no_root_squash,no_subtree_check)' >> /etc/exports
+echo '/srv/nfs 10.132.0.5/32(rw,no_root_squash,no_subtree_check)' >> /etc/exports
 mount -a
 chmod 775 /srv/docker/join.sh
 /srv/docker/join.sh
